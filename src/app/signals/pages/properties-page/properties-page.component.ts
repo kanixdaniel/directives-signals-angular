e992f2be0b929ca24baced36c3e978c9b9466a2a
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-17 17:37:39 -0600

    mutacion del valor de un signal

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
-  templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
+    templateUrl: './properties-page.component.html',
+    styleUrls: ['./properties-page.component.css']
 })
 export class PropertiesPageComponent {
+    user = signal<User>({
+        id: 2,
+        email: "janet.weaver@reqres.in",
+        first_name: "Janet",
+        last_name: "Weaver",
+        avatar: "https://reqres.in/img/faces/2-image.jpg"
+    });
+    fullName = computed<string>(() => `${this.user()!.first_name} ${this.user()!.last_name}`)
 
+    onFieldUpdated(field: keyof User, value: string): void {
+        /* this.user.set({
+            ...this.user(),
+            [field]: value
+        }); */
+
+        /* this.user.update(current => ({
+            ...current,
+            [field]: value
+        })); */
+
+        this.user.update(current => {
+            switch (field) {
+                case 'email':
+                    current.email = value;
+                    break;
+                case 'first_name':
+                    current.first_name = value;
+                    break;
+                case 'last_name':
+                    current.last_name = value;
+                    break;
+            }
+
+            return current
+        })
+    }
 }
