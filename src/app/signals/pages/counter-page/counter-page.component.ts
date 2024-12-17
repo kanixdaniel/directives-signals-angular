5223bc38875dbe66066cfafe928cc9268c46ea34
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2024-12-17 13:34:18 -0600

    formas de cambiar valor del signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
   styleUrls: ['./counter-page.component.css']
 })
 export class CounterPageComponent {
+    counter = signal(10);
+    squareCounter = computed(() => this.counter() * this.counter());
 
+    increaseBy(value: number) {
+        this.counter.update(currentValue => currentValue + value);
+    }
 }
